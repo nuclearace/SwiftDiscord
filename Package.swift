a8c2d82931777aae836e872caf22e7b14267e6c5
Erik <nuclear.ace@gmail.com>
2016-10-30 11:22:55 -0400

    remove unused package

--- Package.swift
+++ Package.swift
@@ -26,7 +26,6 @@ let package = Package(
     ],
     dependencies: [
     	.Package(url: "https://github.com/daltoniam/Starscream", majorVersion: 2),
-    	.Package(url: "https://github.com/nuclearace/copus", majorVersion: 1),
     	.Package(url: "https://github.com/nuclearace/Sodium", majorVersion: 1),
     	.Package(url: "https://github.com/vapor/socks", majorVersion: 1)
     ]
