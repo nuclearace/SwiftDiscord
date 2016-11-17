6eb366c7eec291e4c258cba57297008210794f23
Erik <nuclear.ace@gmail.com>
2016-11-16 20:39:39 -0500

    one line switch statements

--- Sources/SwiftDiscord/DiscordDispatchEventHandler.swift
+++ Sources/SwiftDiscord/DiscordDispatchEventHandler.swift
@@ -40,48 +40,26 @@ public protocol DiscordDispatchEventHandler : DiscordClientSpec {
 public extension DiscordDispatchEventHandler {
 	func handleDispatch(event: DiscordDispatchEvent, data: DiscordGatewayPayloadData) {
 		switch (event, data) {
-		case let (.presenceUpdate, .object(data)):
-			handlePresenceUpdate(with: data)
-		case let (.messageCreate, .object(data)):
-			handleEvent("messageCreate", with: [DiscordMessage(messageObject: data)])
-		case let (.guildMemberAdd, .object(data)):
-			handleGuildMemberAdd(with: data)
-		case let (.guildMembersChunk, .object(data)):
-			handleGuildMembersChunk(with: data)
-		case let (.guildMemberUpdate, .object(data)):
-			handleGuildMemberUpdate(with: data)
-		case let (.guildMemberRemove, .object(data)):
-			handleGuildMemberRemove(with: data)
-		case let (.guildRoleCreate, .object(data)):
-			handleGuildRoleCreate(with: data)
-		case let (.guildRoleDelete, .object(data)):
-			handleGuildRoleRemove(with: data)
-		case let (.guildRoleUpdate, .object(data)):
-			handleGuildRoleUpdate(with: data)
-		case let (.guildCreate, .object(data)):
-			handleGuildCreate(with: data)
-		case let (.guildDelete, .object(data)):
-			handleGuildDelete(with: data)
-		case let (.guildUpdate, .object(data)):
-			handleGuildUpdate(with: data)
-		case let (.guildEmojisUpdate, .object(data)):
-			handleGuildEmojiUpdate(with: data)
-		case let (.channelUpdate, .object(data)):
-			handleChannelUpdate(with: data)
-		case let (.channelCreate, .object(data)):
-			handleChannelCreate(with: data)
-		case let (.channelDelete, .object(data)):
-			handleChannelDelete(with: data)
-		case let (.voiceServerUpdate, .object(data)):
-			handleVoiceServerUpdate(with: data)
-		case let (.voiceStateUpdate, .object(data)):
-			handleVoiceStateUpdate(with: data)
-		case let (.ready, .object(data)):
-			handleReady(with: data)
-		default:
-			// print("Dispatch event went unhandled \(event)\ndelegating to listeners")
-
-			handleEvent(event.rawValue, with: [data])
+		case let (.presenceUpdate, .object(data)):	handlePresenceUpdate(with: data)
+		case let (.messageCreate, .object(data)):	handleEvent("messageCreate", with: [DiscordMessage(messageObject: data)])
+		case let (.guildMemberAdd, .object(data)):	handleGuildMemberAdd(with: data)
+		case let (.guildMembersChunk, .object(data)):	handleGuildMembersChunk(with: data)
+		case let (.guildMemberUpdate, .object(data)):	handleGuildMemberUpdate(with: data)
+		case let (.guildMemberRemove, .object(data)):	handleGuildMemberRemove(with: data)
+		case let (.guildRoleCreate, .object(data)):	handleGuildRoleCreate(with: data)
+		case let (.guildRoleDelete, .object(data)):	handleGuildRoleRemove(with: data)
+		case let (.guildRoleUpdate, .object(data)):	handleGuildRoleUpdate(with: data)
+		case let (.guildCreate, .object(data)):	handleGuildCreate(with: data)
+		case let (.guildDelete, .object(data)):	handleGuildDelete(with: data)
+		case let (.guildUpdate, .object(data)):	handleGuildUpdate(with: data)
+		case let (.guildEmojisUpdate, .object(data)):	handleGuildEmojiUpdate(with: data)
+		case let (.channelUpdate, .object(data)):	handleChannelUpdate(with: data)
+		case let (.channelCreate, .object(data)):	handleChannelCreate(with: data)
+		case let (.channelDelete, .object(data)):	handleChannelDelete(with: data)
+		case let (.voiceServerUpdate, .object(data)):	handleVoiceServerUpdate(with: data)
+		case let (.voiceStateUpdate, .object(data)):	handleVoiceStateUpdate(with: data)
+		case let (.ready, .object(data)):	handleReady(with: data)
+		default:	handleEvent(event.rawValue, with: [data])
 		}
 	}
 }
