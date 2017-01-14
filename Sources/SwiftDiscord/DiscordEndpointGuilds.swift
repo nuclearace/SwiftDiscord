df6134075823a84f5a8bc7db90e74b09e4998f6d
Erik <nuclear.ace@gmail.com>
2017-01-14 11:14:59 -0500

    Add adding roles to guild members

--- Sources/SwiftDiscord/DiscordEndpointGuilds.swift
+++ Sources/SwiftDiscord/DiscordEndpointGuilds.swift
@@ -237,6 +237,32 @@ public extension DiscordEndpoint {
     // Guild Members
 
     /**
+        Adds a role to a guild member.
+
+        - parameter roleId: The id of the role to add.
+        - parameter to: The id of the member to add this role to.
+        - parameter on: The id of the guild this member is on.
+        - parameter with: The token to authenticate to Discord with.
+        - parameter callback: An optional callback indicating whether the role was added successfully.
+    */
+    public static func addGuildMemberRole(_ roleId: String, to userId: String, on guildId: String,
+            with token: DiscordToken, callback: ((Bool) -> Void)?) {
+        var request = createRequest(with: token, for: .guildMemberRole, replacing: [
+            "guild.id": guildId,
+            "user.id": userId,
+            "role.id": roleId
+        ])
+
+        request.httpMethod = "PUT"
+
+        let rateLimiterKey = DiscordRateLimitKey(endpoint: .guildMemberRole, parameters: ["guild.id": guildId])
+
+        DiscordRateLimiter.executeRequest(request, for: rateLimiterKey, callback: {data, response, error in
+           callback?(response?.statusCode == 204)
+        })
+    }
+
+    /**
         Gets the specified guild member.
 
         - parameter by: The snowflake id of the member
