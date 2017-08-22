5851329a879cb8a7998449194fcfb53a23d79cbe
Erik Little <nuclear.ace@gmail.com>
2017-08-22 19:54:15 -0400

    Rename test runner

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,9 +7,14 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install libsodium
     brew install opus
 else
+    # Vapor's scripts take care of Swift installation
     eval "$(curl -sL https://apt.vapor.sh)"
     sudo apt-get install vapor
+
+    # Opus
     sudo apt-get install libopus-dev
+
+    # Sodium
     wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.13.tar.gz
     tar -xzvf libsodium-1.0.13.tar.gz
     cd libsodium-1.0.13
