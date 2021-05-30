8de1887a90f1e1ebe06c747a363e7e51b03a263b
fwcd <fwcdmail@gmail.com>
2021-05-31 01:47:38 +0200

    Add customId to DiscordInteraction

--- Sources/SwiftDiscord/Interaction/DiscordInteraction.swift
+++ Sources/SwiftDiscord/Interaction/DiscordInteraction.swift
@@ -18,6 +18,9 @@ public struct DiscordInteraction {
     /// The message a user interacted with, e.g. when pressing a button.
     public let message: DiscordMessage?
 
+    /// A custom (developer-defined) id attached to e.g. a button interaction.
+    public let customId: String?
+
     /// Guild it was sent from
     public let guildId: GuildID
 
@@ -38,6 +41,7 @@ public struct DiscordInteraction {
         type = (interactionObject["type"] as? Int).flatMap(DiscordInteractionType.init(rawValue:))
         data = (interactionObject["data"] as? [String: Any]).map(DiscordApplicationCommandInteractionData.init(dataObject:))
         message = (interactionObject["message"] as? [String: Any]).map { DiscordMessage(messageObject: $0, client: nil) }
+        customId = interactionObject["custom_id"] as? String
         let guildId = Snowflake(interactionObject["guild_id"] as? String) ?? 0
         self.guildId = guildId
         channelId = Snowflake(interactionObject["channel_id"] as? String) ?? 0
