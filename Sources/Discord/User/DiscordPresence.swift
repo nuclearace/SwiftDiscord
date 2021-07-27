79b930d06f9b70ffe2e0cac9ad87d18302b6d1c9
fwcd <fwcdmail@gmail.com>
2021-07-27 23:23:50 +0200

    Update license headers

--- Sources/Discord/User/DiscordPresence.swift
+++ Sources/Discord/User/DiscordPresence.swift
@@ -1,5 +1,6 @@
 // The MIT License (MIT)
 // Copyright (c) 2016 Erik Little
+// Copyright (c) 2021 fwcd
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 // documentation files (the "Software"), to deal in the Software without restriction, including without
