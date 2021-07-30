bd9d5eb012fc681c9fc5b3d759c5b21f482a39b4
fwcd <fwcdmail@gmail.com>
2021-07-30 15:13:48 +0200

    Define strongly typed gateway command structures (WIP)

--- Sources/Discord/Gateway/DiscordGatewayPayload.swift
+++ Sources/Discord/Gateway/DiscordGatewayPayload.swift
@@ -16,38 +16,34 @@
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+/// The most basic form of event sent and received via Discord's gateway.
 public enum DiscordGatewayPayload: Codable {
-    /// An event was dispatched (receive).
-    /// Receive.
-    case dispatch(DiscordDispatchEvent)
-    /// Fired periodically by the client to keep the connection alive.
-    /// Send/Receive.
-    case heartbeat(DiscordHeartbeat)
+    // MARK: Commands (Send)
+
     /// Starts a new session during the initial handshake.
-    /// Send.
-    case identify(DiscordIdentify)
-    /// Updates the clients presence.
-    /// Send.
-    case presenceUpdate
+    case identify(DiscordGatewayIdentify)
+    /// Fired periodically by the client to keep the connection alive.
+    case heartbeat(DiscordGatewayHeartbeat)
     /// Resumes a previous session that was disconnected.
-    /// Send.
-    case resume(DiscordResume)
-    /// You should attempt to reconnect and resume immediately.
-    /// Receive.
-    case reconnect
+    case resume(DiscordGatewayResume)
+    /// Updates the clients presence.
+    case presenceUpdate(DiscordGatewayPresenceUpdate)
     /// Request information about offline guild members in a large guild.
-    /// Send.
-    case requestGuildMembers
-    /// The session has been invalidated. You should reconnect and identify/
+    case requestGuildMembers(DiscordGatewayRequestGuildMembers)
+
+    // MARK: Events (Receive)
+
+    /// An event was dispatched (receive).
+    case dispatch(DiscordDispatchEvent)
+    /// We should attempt to reconnect and resume immediately.
+    case reconnect
+    /// The session has been invalidated. We should reconnect and identify/
     /// resume accordinly.
-    /// Receive.
     case invalidSession
     /// Sent immediately after connecting, contains the `heartbeat_interval`.
-    /// Receive.
     case hello
     /// Sent in response to receiving a heartbeat to acknowledge that it has
     /// been received.
-    /// Receive.
     case heartbeatAck
 }
 
@@ -79,7 +75,9 @@ public enum DiscordGatewayPayloadOpcode: Int, Codable {
     case heartbeatAck = 11
 }
 
-public struct DiscordHeartbeat: RawRepresentable, Codable {
+/// Used to maintain an active gateway connection. Must be sent
+/// every `heartbeat_interval` ms after the hello.
+public struct DiscordGatewayHeartbeat: RawRepresentable, Codable {
     /// The last sequence number received by the client.
     public var rawValue: Int
 
@@ -88,18 +86,56 @@ public struct DiscordHeartbeat: RawRepresentable, Codable {
     }
 }
 
+#if os(Linux)
+private let currentOS = "Linux"
+#elseif os(macOS)
+private let currentOS = "macOS"
+#elseif os(Windows)
+private let currentOS = "Windows"
+#else
+private let currentOS = "Other"
+#endif
+
 /// An event for identifying itself to the gateway.
-public struct DiscordIdentify: Codable {
+public struct DiscordGatewayIdentify: Codable {
+    public enum CodingKeys: String, CodingKey {
+        case token
+        case intents
+        case properties
+        case compress
+        case largeThreshold = "large_threshold"
+        case shard
+        case presence
+    }
+
     /// The token to use.
     public var token: DiscordToken
-    /// The intents to provide.
+    /// The gateway intents we wish to receive.
     public var intents: DiscordIntent
-    /// Information about the client.
-    public var properties: [String: String]
+    /// Connection properties.
+    public var properties: Properties
+    /// Whether this connection supports compression of packets.
+    public var compress: Bool? = false
+    /// Value between 50 and 250, total number of members where the
+    /// gateway will stop sending offline members in the guild member
+    /// list.
+    public var largeThreshold: Int? = 50
+    /// Array of two integers (shard_id, num_shards)
+    public var shard: [Int]? = nil
+
+    /// Connection properties.
+    public struct Properties: Codable {
+        /// Our operating system.
+        public var os: String = currentOS
+        /// Our library name.
+        public var browser: String = "swift-discord"
+        /// Our library name.
+        public var device: String = "swift-discord"
+    }
 }
 
 /// An event for 'resuming' a connection, replaying lost events.
-public struct DiscordResume: Codable {
+public struct DiscordGatewayResume: Codable {
     public enum CodingKeys: String, CodingKey {
         case token
         case sessionId = "session_id"
@@ -112,4 +148,36 @@ public struct DiscordResume: Codable {
     public var sessionId: String
     /// The sequence number of the last event received.
     public var sequenceNumber: Int
+}
+
+/// Used to request all members for a guild or a list of guilds.
+public struct DiscordGatewayRequestGuildMembers: Codable {
+    /// The id of the guild to get members for.
+    public var guildId: GuildID
+    /// String that username starts with (allows empty string for all).
+    /// Either `query` or `userIds` is needed.
+    public var query: String? = nil
+    /// Used to specify which users we wish to fetch.
+    /// Either `query` or `userIds` is needed.
+    public var userIds: [UserID]? = nil
+    /// Maximum number of members to send matching the `query`.
+    /// Limit of 0 may be used to return all members.
+    public var limit: Int = 0
+    /// Used to specify if we want the presenced of the matched members.
+    public var presences: Bool? = false
+    /// Nonce to identify the guild members chunk response.
+    public var nonce: String? = nil
+}
+
+/// Sent by the client to indicate a presence update.
+public struct DiscordGatewayPresenceUpdate: Codable {
+    /// Unix time in ms of when the client went idle, or
+    /// null if the client is not idle.
+    public var since: Int? = nil
+    /// The user's activities.
+    public var activities: [DiscordActivity]
+    /// The user's new status.
+    public var status: DiscordPresenceStatus
+    /// Whether or not the client is afk.
+    public var afk: Bool
 }
