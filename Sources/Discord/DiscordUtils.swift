745ef1cc6273f4f5790a1e322c78204b5b5b4c94
fwcd <fwcdmail@gmail.com>
2021-07-31 19:45:38 +0200

    Update audit log entry (WIP)

--- Sources/Discord/DiscordUtils.swift
+++ Sources/Discord/DiscordUtils.swift
@@ -202,3 +202,23 @@ public class CodableBox<Value>: Codable where Value: Codable {
         try wrappedValue.encode(to: encoder)
     }
 }
+
+extension KeyedDecodingContainerProtocol {
+    func decodePrimitiveAny(forKey key: Key) throws -> Any {
+        if let b = try? decode(Bool.self, forKey: key) {
+            return b
+        } else if let i = try? decode(Int.self, forKey: key) {
+            return i
+        } else if let s = try? decode(String.self, forKey: key) {
+            return s
+        } else if let d = try? decode(Double.self, forKey: key) {
+            return d
+        } else {
+            throw DecodingError.typeMismatch(Any.self, .init(
+                codingPath: codingPath,
+                debugDescription: "Could not decode primitive any",
+                underlyingError: nil
+            ))
+        }
+    }
+}
